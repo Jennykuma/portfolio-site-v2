5eabcaf981e809a2f105bb937462c1defb98cc21
Jennykuma <jenny@Jennys-MacBook-Air.local>
2025-10-25 13:50:37 -0600

    work on emblacarousel some more

--- src/components/EmblaCarousel.tsx
+++ src/components/EmblaCarousel.tsx
@@ -39,10 +39,14 @@ const EmblaCarousel = (props: PropType) => {
             </div>
             <div className="embla-buttons">
                 <button className="embla-prev" onClick={scrollPrev}>
-                    Prev
+                    <svg className="w-5 h-5 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
+                        <path d="M165.66,202.34a8,8,0,0,1-11.32,11.32l-80-80a8,8,0,0,1,0-11.32l80-80a8,8,0,0,1,11.32,11.32L91.31,128Z"></path>
+                    </svg>
                 </button>
                 <button className="embla-next" onClick={scrollNext}>
-                    Next
+                    <svg className="w-5 h-5 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
+                        <path d="M181.66,133.66l-80,80a8,8,0,0,1-11.32-11.32L164.69,128,90.34,53.66a8,8,0,0,1,11.32-11.32l80,80A8,8,0,0,1,181.66,133.66Z"></path>
+                    </svg>
                 </button>
             </div>
         </div>
