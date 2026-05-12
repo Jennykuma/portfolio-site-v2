d105121c18719c02ea34a7d6933181121b79d572
Jenny <jennyle.dev@gmail.com>
2026-05-11 20:20:16 -0600

    Add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,22 @@ import GetInTouch from './components/GetInTouch';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Photography from './components/Photography';
+import { Analytics } from '@vercel/analytics/react';
 
 const App = () => {
 	return (
-		<div className='font-poppins max-w-2xl mx-auto px-4'>
-			<span id='top'></span>
-			<Nav />
-			<About />
-			<Experience />
-			<Projects />
-			<Photography />
-			<GetInTouch />
-		</div>
+		<>
+			<div className='font-poppins max-w-2xl mx-auto px-4'>
+				<span id='top'></span>
+				<Nav />
+				<About />
+				<Experience />
+				<Projects />
+				<Photography />
+				<GetInTouch />
+			</div>
+			<Analytics />
+		</>
 	);
 };
 
